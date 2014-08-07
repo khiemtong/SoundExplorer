4c016c1f02719903f30f075450c0af8ab1e04f1f
Khiem Tong <tong.khiem@gmail.com>
2014-08-07 18:16:30 -0400

    Plays sounds giving trackId

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 var express = require('express');
+var socketio = require('socket.io');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
 
+var server = app.listen(3000);
 
-app.listen(3000);
+var io = socketio.listen(server);
+io.on('connection', function(socket) {
+
+  console.log('client connected');
+  socket.broadcast.emit('server ready', { 'message' : 'ok' });
+
+});
+
+app.use(bodyParser.json());
+
+app.get('/api/tracks/:id', function(req, res, next) {
+
+      var trackId = req.params.id;
+      io.sockets.emit('new song', { 'trackId' : trackId });
+
+      res.end("success");
+});
